f8bf9b36b66ae8c672017a53be88da9a6cd04bfe
ehabmosilhy <ehabmosilhy@gmail.com>
2022-11-07 03:42:42 +0200

    limit customers in POS

---
+++ azba/azbah_pos/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Azbah - POS Changes",
+    "version": "15.1",
+    "license": "LGPL-3",
+    "category": "POS Changes for Azba",
+    "depends": ["base",  "point_of_sale"],
+    "data": [
+
+             ],
+}
