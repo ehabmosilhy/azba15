3f3f2c4e55a47f2bd913e2666428642efaf3d6e8
ehabmosilhy <ehabmosilhy@gmail.com>
2022-11-08 16:41:55 +0200

    6

--- azba/azbah_trivial/models/res_partner.py
+++ azba/azbah_trivial/models/res_partner.py
@@ -3,7 +3,7 @@ from odoo import fields, models, api
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
-    date_relation_start = fields.Char(string="تاريخ بداية التعامل")
+    date_relation_start = fields.Date(string="تاريخ بداية التعامل")
     @api.depends('is_company', 'name', 'parent_id.display_name', 'type', 'company_name', 'code')
     def _compute_display_name(self):
         for partner in self:
