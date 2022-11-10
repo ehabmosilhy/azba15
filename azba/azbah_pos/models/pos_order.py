617f8ce142e0fc5cef2bc33cc9ef1941542e3957
ehabmosilhy <ehabmosilhy@gmail.com>
2022-11-10 16:01:26 +0200

    solved automatic invoice

--- azba/azbah_pos/models/pos_order.py
+++ azba/azbah_pos/models/pos_order.py
@@ -22,51 +22,8 @@ _logger = logging.getLogger(__name__)
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
-    invoice_id = fields.Char()
-
     @api.model
-    def _process_order(self, order, draft, existing_order):
-        """Create or update an pos.order from a given dictionary.
-
-        :param dict order: dictionary representing the order.
-        :param bool draft: Indicate that the pos_order is not validated yet.
-        :param existing_order: order to be updated or False.
-        :type existing_order: pos.order.
-        :returns: id of created/updated pos.order
-        :rtype: int
-        """
-        order = order['data']
-        pos_session = self.env['pos.session'].browse(order['pos_session_id'])
-        if pos_session.state == 'closing_control' or pos_session.state == 'closed':
-            order['pos_session_id'] = self._get_valid_session(order).id
-
-        pos_order = False
-        if not existing_order:
-            pos_order = self.create(self._order_fields(order))
-        else:
-            pos_order = existing_order
-            pos_order.lines.unlink()
-            order['user_id'] = pos_order.user_id.id
-            pos_order.write(self._order_fields(order))
-
-        pos_order = pos_order.with_company(pos_order.company_id)
-        self = self.with_company(pos_order.company_id)
-        self._process_payment_lines(order, pos_order, pos_session, draft)
-
-        if not draft:
-            try:
-                pos_order.action_pos_order_paid()
-            except psycopg2.DatabaseError:
-                # do not hide transactional errors, the order(s) won't be saved!
-                raise
-            except Exception as e:
-                _logger.error('Could not fully process the POS Order: %s', tools.ustr(e))
-            pos_order._create_order_picking()
-            pos_order._compute_total_cost_in_real_time()
-
-        # Ehab
-        # Always Make an Invoice with each POS order
-        if pos_order.state == 'paid':
-            pos_order._generate_pos_order_invoice()
-
-        return pos_order.id
+    def _order_fields(self, ui_order):
+        order_fields = super(PosOrder, self)._order_fields(ui_order)
+        order_fields['to_invoice'] = True
+        return order_fields
