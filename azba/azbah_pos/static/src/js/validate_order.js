000f0b39e49337e0ba638b1432193ea2c0b1121c
ehabmosilhy <ehabmosilhy@gmail.com>
2022-11-25 18:48:39 +0200

    pos order: client code

--- azba/azbah_pos/static/src/js/validate_order.js
+++ azba/azbah_pos/static/src/js/validate_order.js
@@ -10,11 +10,19 @@ odoo.define('azbah_pos.validate', function (require) {
              */
             async validateOrder(isForceValidate) {
                 // Set the current order to (to_invoice) if it has lines
-                if (this.currentOrder.get_orderlines().length > 0) {
-                    this.currentOrder.set_to_invoice(true);
-                    this.render();
+                // Ehab
+                // Check Connection
+                let connection_status = this.env.pos.get('synch').status;
+                if (connection_status==='connected') {
+                    if (this.currentOrder.get_orderlines().length > 0) {
+                        this.currentOrder.set_to_invoice(true);
+                        this.render();
+                    }
+                    await super.validateOrder(isForceValidate);
                 }
-                await super.validateOrder(isForceValidate);
+                else {
+                     throw 'No Internet! \n لا يوجد اتصال بالإنترنت';
+                }
 
             }
         };
