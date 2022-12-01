86bc5d2eb1abc98d2558de023795b9cf67f42fd7
ehabmosilhy <ehabmosilhy@gmail.com>
2022-12-01 16:38:15 +0200

    removed payment screen customization

--- azba/azbah_pos/static/src/js/customer_fields.js
+++ azba/azbah_pos/static/src/js/customer_fields.js
@@ -5,11 +5,3 @@ odoo.define('azbah_pos.customer_fields', function (require) {
     models.load_fields("res.partner", ["code", "english_name"]);
     models.load_fields("pos.order", ["id"]);
 })
-    // var super_order_model = models.Order.prototype;
-    // models.Order = models.Order.extend({
-    //     initialize: function (attributes, options) {
-    //         super_order_model.initialize.apply(this, arguments);
-    //         if (!options.json) {
-    //             this.employee = this.pos.get_cashier();
-    //         }
-    //     }});
