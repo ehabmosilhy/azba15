87ce6e76a24e57b30870a83dbe8fb3efe917a5ee
ehabmosilhy <ehabmosilhy@gmail.com>
2022-12-18 09:09:31 +0200

    fixed pos receipt

--- azba/azbah_pos/static/src/js/invoice_number.js
+++ azba/azbah_pos/static/src/js/invoice_number.js
@@ -1,67 +1,60 @@
-odoo.define('azbah_pos.invoice_number', function (require) {
+odoo.define('azbah_pos.invoice_number',function(require){
     "use strict"
 
     let model = require("point_of_sale.models");
     let SuperPosModel = model.PosModel.prototype;
     let SuperOrder = model.Order.prototype;
     let rpc = require('web.rpc');
-    let _super_order = model.Order.prototype;
-
-
 
     model.PosModel = model.PosModel.extend({
-        _flush_orders: function (orders, options) {
+        _flush_orders: function(orders, options) {
             let self = this;
             let result, data
-            result = data = SuperPosModel._flush_orders.call(this, orders, options)
-            _.each(orders, function (order) {
+            result = data = SuperPosModel._flush_orders.call(this,orders, options)
+            _.each(orders,function(order){
                 if (order.to_invoice)
-                    data.then(function (order_server_id) {
+                    data.then(function(order_server_id){
                         rpc.query({
-                            model: 'pos.order',
-                            method: 'read',
-                            args: [order_server_id, ['account_move']]
-                        }).then(function (result_dict) {
-                            if (result_dict.length) {
-                                // let invoice = result_dict[0].account_move;
-                                // self.get_order().invoice_number = invoice[1]
+                        model: 'pos.order',
+                        method: 'read',
+                        args:[order_server_id, ['account_move']]
+                            }).then(function(result_dict){
+                                if(result_dict.length){
+                                    // let invoice = result_dict[0].account_move;
+                                    // self.get_order().invoice_number = invoice[1]
 
-                                let invoice = result_dict[0].account_move;
-                                let order_id = result_dict[0].id;
+                                    let invoice = result_dict[0].account_move;
+                                    let order_id = result_dict[0].id;
 
-                                self.get_order().invoice_number = invoice[1]
-                                self.get_order().id = order_id
-                            }
+                                    self.get_order().invoice_number = invoice[1]
+                                    self.get_order().id = order_id
+                                }
                         })
-                            .catch(function (error) {
-                                return result
-                            })
+                        .catch(function(error){
+                            return result
+                        })
                     })
             })
             return result
         },
     })
-
-    // Ehab
     model.Order = model.Order.extend({
-          initialize: function() {
-        _super_order.initialize.apply(this,arguments);
-        this.invoice_name = this.invoice_name || false;
-        this.save_to_db();
-    },
-    export_for_printing: function() {
-      var result = _super_order.export_for_printing.apply(this,arguments);
-      result.invoice_name = this.get_invoice_name();
-      return result;
-    },
-    get_invoice_name: function() {
-      return this.invoice_name;
-    },
-
+        export_for_printing: function(){
+            let self = this
+            let receipt = SuperOrder.export_for_printing.call(this)
+            if(self.invoice_number){
+                receipt.invoice_number = self.invoice_number.split(" ")[0];
+                receipt.id = self.id;
+            }
+            return receipt
+        }
     })
+    model.Order = models.Order.extend({
 
-
-
+        // Ehab
+        get_invoice_name: function () {
+            alert(this.invoice_name);
+            return this.invoice_name;
+        },
+    });
 })
-
-
