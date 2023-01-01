72c9d743b4a0be62a694d7f8f70d5efceeb93def
ehabmosilhy <ehabmosilhy@gmail.com>
2023-01-01 09:17:12 +0200

    removed pragmatic

--- azba/azbah_accounting/__manifest__.py
+++ azba/azbah_accounting/__manifest__.py
@@ -5,6 +5,13 @@
     "category": "accounting",
     "depends": ["base", "account_accountant"],
     "data": [
-        'payments/account_payment_view.xml'
-             ],
+        'payments/account_payment_view.xml',
+        'payments/batch_vendor_bill.xml',
+        'security/account_batch_bill.xml'
+
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'azbah_accounting/static/src/js/*.js',
+        ], }
 }
--- azba/azbah_accounting/payments/__init__.py
+++ azba/azbah_accounting/payments/__init__.py
@@ -1,1 +1,3 @@
 from . import account_payment
+from . import batch_vendor_bill
+from . import account_move
