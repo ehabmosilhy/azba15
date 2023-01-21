fe8fe5cf565d6034417f18cec0f7fb2c54f07720
ehabmosilhy <ehabmosilhy@gmail.com>
2023-01-21 09:48:29 +0200

    emps

--- azba/azbah_hr/models/hr_employee.py
+++ azba/azbah_hr/models/hr_employee.py
@@ -5,5 +5,6 @@ class HREmployee(models.AbstractModel):
     _inherit = 'hr.employee.base'
     visa_issue_date = fields.Date()
     work_start_date = fields.Date()
-
-
+    medical_certificate_number = fields.Char()
+    medical_certificate_number_issue_date = fields.Date()
+    medical_certificate_number_expiration_date = fields.Date()
