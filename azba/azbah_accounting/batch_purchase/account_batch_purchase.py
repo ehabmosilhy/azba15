c2219c4fb5d22c768d193d9dd4b5ef9eba29484c
ehabmosilhy <ehabmosilhy@gmail.com>
2023-02-16 12:51:10 +0200

    vendor sections

--- azba/azbah_accounting/batch_purchase/account_batch_purchase.py
+++ azba/azbah_accounting/batch_purchase/account_batch_purchase.py
@@ -16,10 +16,23 @@ class BatchPurchase(models.Model):
                                 string='Vendor', change_default=True, domain=[('code', 'ilike', 'v%')])
 
     line_ids = fields.One2many('account.batch.purchase.line', 'batch_id')
+    line_count = fields.Integer(compute='_compute_line_count', string='Line count')
+
+    @api.depends('line_ids')
+    def _compute_line_count(self):
+        for record in self:
+            record.line_count = len(record.line_ids)
 
     # Update the Total
     @api.onchange('line_ids')
     def onchange_price_or_qty(self):
+        if self.line_count < 2 and self.line_ids and self.line_ids[-1].product_id and not self.line_ids[-1].vendor_id:
+            self.line_ids[-1].unlink()
+            warning = {
+                'message': "You must select vendor first"
+            }
+            return {'warning': warning}
+
         total = 0
         for line in self.line_ids:
             total += line.price_subtotal_with_tax
@@ -33,16 +46,20 @@ class BatchPurchase(models.Model):
         # We need to group the lines by vendor to make all the lines of one vendor together in
         # one purchase order
         if batch_lines:
-            for batch_line in batch_lines:
-                line = batch_line[2]
-                # if the user doesn't enter a note, make it empty without the HTML characters
-                if '<br' in line['note']:
-                    line['note'] = ""
-                vendor_id = line.get('vendor_id')
-                if vendor_id in orders.keys():
-                    orders[vendor_id].append(line)
-                else:
-                    orders[vendor_id] = [line]
+            for i in range(len(batch_lines)):
+                line = batch_lines[i][2]
+                if not line.get('display_type'):
+                    # if the user doesn't enter a note, make it empty without the HTML characters
+                    if '<br' in line['note']:
+                        line['note'] = ""
+                    vendor_id = line.get('vendor_id')
+                    if not vendor_id:
+                        vendor_id = batch_lines[i - 1][2].get('vendor_id')
+                        batch_lines[i][2]['vendor_id'] = vendor_id
+                    if vendor_id in orders.keys():
+                        orders[vendor_id].append(line)
+                    else:
+                        orders[vendor_id] = [line]
 
         for order in orders.items():
             if vals_list.get('vendor_count') == 'single':
@@ -94,9 +111,10 @@ class BatchPurchase(models.Model):
 class BatchVendorBillLine(models.Model):
     _name = "account.batch.purchase.line"
     batch_id = fields.Many2one('account.batch.purchase')
+    line_count = fields.Integer(related='batch_id.line_count')
+
     vendor_id = fields.Many2one('res.partner', tracking=True,
                                 string='Vendor', change_default=True, domain=[('code', 'ilike', 'v%')])
-    vendor_count = fields.Selection(related="batch_id.vendor_count")
     product_id = fields.Many2one('product.product', string='Product', ondelete='restrict')
     product_uom_category_id = fields.Many2one(related='product_id.uom_id.category_id')
     product_uom = fields.Many2one('uom.uom', string='Unit of Measure',
@@ -106,6 +124,7 @@ class BatchVendorBillLine(models.Model):
     price_subtotal = fields.Float(string="Sub Total")
     price_subtotal_with_tax = fields.Float(string="With Tax")
     note = fields.Html()
+    display_type = fields.Char()
     tax_ids = fields.Many2many(
         comodel_name='account.tax',
         string="Taxes",
@@ -117,3 +136,12 @@ class BatchVendorBillLine(models.Model):
     def onchange_price_or_qty(self):
         self.price_subtotal = float(self.price) * float(self.quantity)
         self.price_subtotal_with_tax = float(self.price_subtotal) * 1.15
+
+    # @api.onchange('product_id')
+    # def onchange_product(self):
+    #     if self.product_id and not self.vendor_id:
+    #         self.batch_id.line_ids[-1].unlink()
+    #         warning = {
+    #             'message': "You must select vendor first"
+    #         }
+    #         return {'warning': warning}
