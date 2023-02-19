cba041e906b4c1c97c112b6a298f8d085182be21
ehabmosilhy <ehabmosilhy@gmail.com>
2023-02-19 20:30:13 +0200

    batch purchase

--- azba/azbah_accounting/batch_purchase/res_partner.py
+++ azba/azbah_accounting/batch_purchase/res_partner.py
@@ -9,9 +9,11 @@ class ResPartner(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         source = self.env.context.get('source')
-        if source == 'vendor':
+        if source == 'vendor' or vals_list[0].get('is_company'):
             last_vendor = self.env['res.partner'].search([('code', 'ilike', 'v%')], order='id desc', limit=1)
             if last_vendor:
                 new_code = 'V'+str(int(last_vendor.code[1:]) + 1).zfill(4)
                 vals_list[0]['code'] = new_code
+                vals_list[0]['company_type'] = 'company'
+                vals_list[0]['is_company'] = True
                 return super().create(vals_list)
