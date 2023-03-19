604b4e4ba020004d1e39d9da5498c9b74af1a2e9
Ehab Mosilhy <ehabmosilhy@gmail.com>
2023-03-19 19:35:43 +0300

    working

--- azba/az_account_payments/models/account_payment.py
+++ azba/az_account_payments/models/account_payment.py
@@ -28,108 +28,7 @@ class AccountPayment(models.Model):
                 'inbound-supplier': "",
             }
 
-    # -------------------------------------------------------------------------
-    # SYNCHRONIZATION account.payment <-> account.move
-    # -------------------------------------------------------------------------
 
-    def _synchronize_from_moves(self, changed_fields):
-        ''' Update the account.payment regarding its related account.move.
-        Also, check both models are still consistent.
-        :param changed_fields: A set containing all modified fields on account.move.
-        '''
-        if self._context.get('skip_account_move_synchronization'):
-            return
-
-        for pay in self.with_context(skip_account_move_synchronization=True):
-
-            # After the migration to 14.0, the journal entry could be shared between the account.payment and the
-            # account.bank.statement.line. In that case, the synchronization will only be made with the statement line.
-            if pay.move_id.statement_line_id:
-                continue
-
-            move = pay.move_id
-            move_vals_to_write = {}
-            payment_vals_to_write = {}
-
-            if 'journal_id' in changed_fields:
-                if pay.journal_id.type not in ('bank', 'cash'):
-                    raise UserError(_("A payment must always belongs to a bank or cash journal."))
-
-            if 'line_ids' in changed_fields:
-                all_lines = move.line_ids
-
-                # -------------------------------------------------------------------------
-                # Azbah
-                # -------------------------------------------------------------------------
-
-                # if self.env.context.get('sanad'):
-                #     writeoff_lines = None
-                #     liquidity_lines, counterpart_lines = all_lines
-                # else:
-                #     liquidity_lines, counterpart_lines, writeoff_lines = pay._seek_for_lines()
-                liquidity_lines, counterpart_lines, writeoff_lines = pay._seek_for_lines()
-
-                if len(liquidity_lines) != 1:
-                    raise UserError(_(
-                        "Journal Entry %s is not valid. In order to proceed, the journal items must "
-                        "include one and only one outstanding payments/receipts account.",
-                        move.display_name,
-                    ))
-
-                if len(counterpart_lines) != 1:
-                    raise UserError(_(
-                        "Journal Entry %s is not valid. In order to proceed, the journal items must "
-                        "include one and only one receivable/payable account (with an exception of "
-                        "internal transfers).",
-                        move.display_name,
-                    ))
-
-                if writeoff_lines and len(writeoff_lines.account_id) != 1:
-                    raise UserError(_(
-                        "Journal Entry %s is not valid. In order to proceed, "
-                        "all optional journal items must share the same account.",
-                        move.display_name,
-                    ))
-
-                if any(line.currency_id != all_lines[0].currency_id for line in all_lines):
-                    raise UserError(_(
-                        "Journal Entry %s is not valid. In order to proceed, the journal items must "
-                        "share the same currency.",
-                        move.display_name,
-                    ))
-
-                if any(line.partner_id != all_lines[0].partner_id for line in all_lines):
-                    raise UserError(_(
-                        "Journal Entry %s is not valid. In order to proceed, the journal items must "
-                        "share the same partner.",
-                        move.display_name,
-                    ))
-
-                if counterpart_lines.account_id.user_type_id.type == 'receivable':
-                    partner_type = 'customer'
-                else:
-                    partner_type = 'supplier'
-
-                liquidity_amount = liquidity_lines.amount_currency
-
-                move_vals_to_write.update({
-                    'currency_id': liquidity_lines.currency_id.id,
-                    'partner_id': liquidity_lines.partner_id.id,
-                })
-                payment_vals_to_write.update({
-                    'amount': abs(liquidity_amount),
-                    'partner_type': partner_type,
-                    'currency_id': liquidity_lines.currency_id.id,
-                    'destination_account_id': counterpart_lines.account_id.id,
-                    'partner_id': liquidity_lines.partner_id.id,
-                })
-                if liquidity_amount > 0.0:
-                    payment_vals_to_write.update({'payment_type': 'inbound'})
-                elif liquidity_amount < 0.0:
-                    payment_vals_to_write.update({'payment_type': 'outbound'})
-
-            move.write(move._cleanup_write_orm_values(move, move_vals_to_write))
-            pay.write(move._cleanup_write_orm_values(pay, payment_vals_to_write))
 
     @api.depends('available_payment_method_line_ids')
     def _compute_payment_method_line_id(self):
@@ -138,11 +37,20 @@ class AccountPayment(models.Model):
         '''
 
         for pay in self:
-        
-            # '''AZBA'''
+
+            #  /\_/\
+            # ( o.o )
+            #  > ^ <
+            # Beginning: Ehab
+            # The payment method gets recalculated after the creation of the payment
             if self.env.context.get('sanad'):
                 pay.payment_method_line_id = 2 if self.env.context.get('default_payment_type')=='outbound' else 1
-            #  ENd '''
+
+            #  (\_/)
+            #  (^.^)
+            #   \_/
+            # Ending of the code
+
             else:
                 available_payment_method_lines = pay.available_payment_method_line_ids
         
@@ -168,23 +76,29 @@ class AccountMove(models.Model):
 
     def write(self, vals):
         if self.env.context.get('sanad'):
-            # get cash account id
-            cash_account_id = self.env['account.account'].search([('code', '=', '1201001')])
+            #  /\_/\
+            # ( o.o )
+            #  > ^ <
+            # The two parties of the move will be : 1- Cash and 2- Whatever account bound to the journal
+            # The other account of the move must fulfill these conditions:
+            #  1- Internal Type: "receivable" or "payable"
+            #  2- Not a liquidity account
+            #  3- Bound to the journal (sanad_account_id)
 
-            # 102011 Accounts Receivable
-            # 201002 Payables
+            cash_account_id = self.env['account.account'].search([('code', '=', '1201001')])
+            sanad_account_id = self.journal_id.sanad_account_id
             if vals.get('line_ids'):
                 for line in vals.get('line_ids'):
                     if self.env.context.get('default_payment_type') == 'outbound':
                         if line[2]['credit'] >0:
                             line[2]['account_id'] = cash_account_id.id
                         else:
-                            line[2]['account_id'] = 742  # self.journal_id.inbound_payment_method_line_ids[0].payment_account_id.id
+                            line[2]['account_id'] = sanad_account_id.id
                     else:
                         if line[2]['debit'] >0:
                             line[2]['account_id'] = cash_account_id.id
                         else:
-                            line[2]['account_id'] = 742
+                            line[2]['account_id'] = sanad_account_id.id
 
         res = super(AccountMove, self).write(vals)
         return res
