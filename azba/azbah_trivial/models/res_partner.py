012bac3e07d729703878e9cf241e10df9f97a365
Ehab Mosilhy <ehabmosilhy@gmail.com>
2023-04-04 14:27:16 +0300

    main config

--- azba/azbah_trivial/models/res_partner.py
+++ azba/azbah_trivial/models/res_partner.py
@@ -16,6 +16,7 @@ class ResPartner(models.Model):
     display_name = fields.Char(compute='_compute_display_name', store=True, readonly=False)
 
     pos_config_ids = fields.Many2many("pos.config", string="Routes المسارات")
+    main_config_id=fields.Many2one("pos.config",string="Main Config المسار الرئيسى")
 
     _sql_constraints = [('code_uniq', 'unique (code)', """Code must be unique هذا الكود موجود من قبل!""")]
 
@@ -28,23 +29,34 @@ class ResPartner(models.Model):
                 result.append((rec.id, '%s' % (rec.name)))
         return result
 
-    @api.onchange('pos_config_ids')
-    def onchange_pos_config_ids(self):
-
-        configs = self.env['pos.config'].search([])
-
+    def map_config_to_tags(self,configs):
         # Add all configs to categories table
         for conf in configs:
             cat_id = self.env['res.partner.category'].search([('name', '=', conf.name)])
             if not cat_id:
                 cat_id = self.env['res.partner.category'].create({'name': conf.name})
-        # partners = self.env['res.partner'].search([])
+
+    @api.onchange('main_config_id')
+    def onchange_main_config_id(self):
+        configs = self.env['pos.config'].search([])
+        all_config_names = configs.mapped("name")
+
+        # make sure all configs exist in categories
+        self.map_config_to_tags(configs)
+
         for rec in self:
-            cat_ids = [cat_id.id for cat_id in rec.category_id if cat_id.name not in configs.mapped("name")]
-            for conf in rec.pos_config_ids:
+            # get non pos.config tags, which are not pos.configs ليست مسارات
+            cat_ids = [cat_id.id for cat_id in rec.category_id if cat_id.name not in all_config_names]
+
+            # Add the main config
+            for conf in rec.main_config_id:
                 cat_id = self.env['res.partner.category'].search([('name', '=', conf.name)])
                 if not cat_id:
                     cat_id = self.env['res.partner.category'].create({'name': conf.name})
                 cat_ids.append(cat_id.id)
             cat_ids = list(set(cat_ids))
             rec.category_id = [(6, 0, cat_ids)]
+
+
+
+
