756a65378df87f77a5d963b652c14350f1bd5475
Ehab Mosilhy <ehabmosilhy@gmail.com>
2023-04-28 16:35:10 +0300

    clever

--- misc/clever/controllers/main.py
+++ misc/clever/controllers/main.py
@@ -8,4 +8,10 @@ class UpdateController(http.Controller):
     def update(self, **kwargs):
         data = http.request.httprequest.data
         data=data.decode("utf-8", "ignore")
-        return f"Thank you very much, we have received the following data\n {data}"
+        return f"Update Endpoint \n Thank you very much, we have received the following data\n {data}"
+
+    @http.route('/api/record_purchase', auth='public', csrf=False, methods=['POST', 'GET'])
+    def record_purchase(self, **kwargs):
+        data = http.request.httprequest.data
+        data=data.decode("utf-8", "ignore")
+        return f"Record Purchase Endpoint \n Thank you very much, we have received the following data\n {data}"
