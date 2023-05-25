cf63e5c56debecd0546dc3b9504bbe867c9936ec
ebrahiem abdellatef <hemabibo68@yahoo.com>
2023-05-26 00:54:58 +0200

    Add New Batch Module 26520231254AM

---
+++ azba/az_account_batch_purchase_financial_movements/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "AZ-Accounting - Batch Purchase Financial Movements ",
+    "version": "15",
+    "license": "LGPL-3",
+    "category": "accounting",
+    "depends": ["account","purchase","az_account_payments"]
+    , "data": [
+    'views/batch_purchase.xml',
+    'views/purchase_order.xml',
+    'report/batch_purchase.xml',
+    'report/batch_purchase_template.xml',
+    'security/batch_purchase.xml',
+]
+}
---
+++ azba/az_account_batch_purchase_financial_movements/models/batch_purchase.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class BatchPurchaseFinancial(models.Model):
+    _name = "batch.purchase.financial"
+    name = fields.Char(string="Name", copy=False, readonly=True,
+                       default="New",
+                       index=True, help="Unique name for the batch purchase with prefix DPO_")
+
+    delegate_id = fields.Many2one('hr.employee', required=True)
+    date = fields.Date(required=True, default=fields.Date.context_today)
+    total = fields.Float()
+    line_ids = fields.One2many('batch.purchase.financial.line', 'batch_id')
+    line_count = fields.Integer(compute='_compute_line_count', string='Line count')
+    purchase_order_ids = fields.One2many('purchase.order', 'batch_purchase_id', string="Purchase Orders")
+    purchase_order_count = fields.Integer(string='Purchase Order Count', compute='_compute_purchase_order_count')
+
+    def _compute_purchase_order_count(self):
+        for record in self:
+            record.purchase_order_count = len(record.purchase_order_ids)
+
+    def launch_purchase_orders(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Purchase Orders',
+            'view_mode': 'tree,kanban,form,pivot,graph,calendar,activity',
+            'res_model': 'purchase.order',
+            'domain': [('batch_purchase_id', '=', self.id)],
+            'context': "{'create': False}"
+        }
+
+    @api.depends('line_ids')
+    def _compute_line_count(self):
+        for record in self:
+            record.line_count = len(record.line_ids)
+
+    # Update the Total
+    @api.onchange('line_ids')
+    def onchange_price_or_qty(self):
+        if self.line_count < 2 and self.line_ids and self.line_ids[-1].product_id and not self.line_ids[-1].vendor_id:
+            self.line_ids[-1].unlink()
+            warning = {
+                'message': "You must select vendor first"
+            }
+            return {'warning': warning}
+
+        total = 0
+        for line in self.line_ids:
+            if not line.display_type:
+                total += line.price_subtotal_with_tax
+
+        self.total = total
+
+        # Calculate sub_total
+        if self.line_count > 1:
+            for indi, line in enumerate(self.line_ids):
+                if line.vendor_id:
+                    sub_total = 0
+                    for child_id in range(indi + 1, len(self.line_ids)):
+                        if not self.line_ids[child_id].display_type:
+                            sub_total += self.line_ids[child_id].price_subtotal_with_tax
+                        else:
+                            break
+                    line.price_subtotal_with_tax = sub_total
+
+    def check_data(self, vals_list):
+        if not vals_list.get('total') > 0:
+            warning = {
+                'message': "Please, check the data"
+            }
+            raise Exception(f'warning  {warning}')
+
+    @api.model
+    def create(self, vals_list):
+        self.check_data(vals_list)
+
+        purchase_orders = {}
+        batch_lines = vals_list.get('line_ids')
+
+        # We need to group the lines by vendor to make all the lines of one vendor together in
+        # one purchase purchase_order
+        if batch_lines:
+            for i in range(len(batch_lines)):
+                line = batch_lines[i][2]
+                if not line.get('display_type'):
+                    vendor_id = line.get('vendor_id')
+                    if not vendor_id:
+                        vendor_id = batch_lines[i - 1][2].get('vendor_id')
+                        batch_lines[i][2]['vendor_id'] = vendor_id
+                    if vendor_id in purchase_orders.keys():
+                        purchase_orders[vendor_id].append(line)
+                    else:
+                        purchase_orders[vendor_id] = [line]
+
+        if vals_list.get('name', "New") == 'New':
+            last_dpo = self.env['batch.purchase'].search([], order='id desc', limit=1)
+            if last_dpo:
+                new_name = 'DPO_' + str(int(last_dpo.name[4:]) + 1).zfill(5)
+            else:
+                new_name = "DPO_00001"
+            vals_list['name'] = new_name
+        batch = super(BatchPurchaseFinancial, self).create(vals_list)
+
+        for purchase_order in purchase_orders.items():
+            vendor_id = purchase_order[0]
+            bill_lines = purchase_order[1]
+
+            new_purchase_order = {
+                "priority": "0",
+                "batch_purchase_id": batch.id,
+                'partner_id': vendor_id,
+                "delegate_id": vals_list['delegate_id'],
+                "currency_id": 148,
+                "picking_type_id": 551
+                , 'date_order': vals_list['date']
+                , 'date_planned': vals_list['date']
+                , 'order_line': [
+                    (0, 0,
+                     {
+                         "sequence": 10
+                         , 'product_id': _line['product_id']
+                         , 'note': _line['note']
+                         , "date_planned": vals_list['date']
+                         , "product_uom": _line['product_uom']
+                         , 'price_unit': _line['price']
+                         , 'product_qty': _line['quantity']
+                     }) for _line in bill_lines]
+            }
+            # Create the purchase purchase_order
+            _new_purchase_order = self.env['purchase.order'].create(new_purchase_order)
+
+            # Confirm the purchase_order
+            _new_purchase_order.button_confirm()
+
+        return batch
+
+
+class BatchPurchaseFinancialLine(models.Model):
+    _name = "batch.purchase.financial.line"
+    batch_id = fields.Many2one('batch.purchase.financial')
+    line_count = fields.Integer(related='batch_id.line_count')
+
+    vendor_id = fields.Many2one('res.partner',
+                                string='Vendor', domain=[('code', 'ilike', 'v%')]
+                                , context={'source': 'vendor'}
+                                )
+    product_id = fields.Many2one('product.product', string='Product', ondelete='restrict')
+    product_uom_category_id = fields.Many2one(related='product_id.uom_id.category_id')
+    product_uom = fields.Many2one('uom.uom', string='Unit of Measure',
+                                  domain="[('category_id', '=', product_uom_category_id)]")
+    quantity = fields.Float()
+    price = fields.Float()
+    price_subtotal = fields.Float(string="Sub Total")
+    price_subtotal_with_tax = fields.Float(string="With Tax")
+    note = fields.Text()
+    display_type = fields.Char()
+    tax_ids = fields.Many2many(
+        comodel_name='account.tax',
+        string="Taxes",
+        help="Taxes that apply on the base amount"
+        , default=[5]  # Purchase Tax 15%
+    )
+
+    @api.onchange('price', 'quantity', 'tax_ids')
+    def onchange_price_or_qty(self):
+        self.price_subtotal = float(self.price) * float(self.quantity)
+        if self.tax_ids:
+            total_tax = 0
+            for tax in self.tax_ids:
+                total_tax += self.price_subtotal * tax.amount / 100
+            self.price_subtotal_with_tax = float(self.price_subtotal) + total_tax
+        else:
+            self.price_subtotal_with_tax = float(self.price_subtotal)
+
+    @api.onchange('vendor_id')
+    def onchange_vendor_id(self):
+        # Don't repeat vendors
+        vendor_ids = self.batch_id.line_ids.mapped('vendor_id')
+        domain = [('code', 'ilike', 'v%'), ('id', 'not in', vendor_ids.ids)]
+        return {'domain': {'vendor_id': domain}}
