6d1a98cb9e68a1a44f261bc66e7356283c03da7d
Ehab Mosilhy <ehabmosilhy@gmail.com>
2023-10-22 14:49:40 +0300

    az_temp bank statement new amount

---
+++ azba/az_temporary/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Azbah - Temp",
+    "version": "15.0",
+    "license": "LGPL-3",
+    "depends": ["base","account"],
+    "data": [
+        "views/bank_statement.xml",
+    ],
+}
---
+++ azba/az_temporary/models/edit_amount.py
@@ -0,0 +1,25 @@
+from odoo import fields, models, api
+
+
+class AccountBankStatement(models.Model):
+    _inherit = 'account.bank.statement'
+    new_amount = fields.Char("القيمة الجديدة")
+
+    def button_edit_amount(self):
+        if self.new_amount:
+            sql = f"update account_bank_statement_line set amount={self.new_amount} where statement_id={self.id}; "
+            sql2 = """
+                    UPDATE account_bank_statement AS s
+                    SET balance_end = (
+                    SELECT SUM(amount)
+                    FROM account_bank_statement_line AS l
+                    WHERE l.statement_id = s.id
+                   ) + s.balance_start;
+            """
+            sql3 = """
+            update account_bank_statement set balance_end_real=balance_end; -- where balance_end_real<>balance_end;
+            """
+
+            self.env.cr.execute(sql)
+            self.env.cr.execute(sql2)
+            self.env.cr.execute(sql3)
