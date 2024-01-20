3ec3fd7d160a96ab7392b65e887eb6fd22f4ee52
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-01-20 08:16:30 +0200

    partners

--- azba/az_account_reports/report/ledger_report_view.py
+++ azba/az_account_reports/report/ledger_report_view.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api, fields, _
-import operator
-from datetime import datetime, date
-from odoo.tools.float_utils import float_round
+from odoo import models, api
 
 
 class ledgerReportTemplate(models.AbstractModel):
@@ -17,47 +14,90 @@ class ledgerReportTemplate(models.AbstractModel):
         end_date = data.get('date_to')
 
         # Depending on the report type, construct the account IDs SQL
-        account_ids = data.get('account_ids')
+        account_ids = data.get('account_ids')[0] if  data.get('account_ids') else None
         account_ids_clause = ""
         params = [company_id]
 
         if account_ids:
-            account_ids_clause = "AND aml.account_id IN %s"
-            params.append(tuple(account_ids.ids))
+            account_move_sql = f"""
+                SELECT
+                    aa.name AS account_name,
+                    p.code,
+                    p.name AS name,
+                    SUM(aml.debit) AS period_debit,
+                    SUM(aml.credit) AS period_credit,
+                    COALESCE(pd.total_debit_before, 0) AS before_period_debit,
+                    COALESCE(pd.total_credit_before, 0) AS before_period_credit,
+                    aml.account_id,
+                    0 as final_debit,
+                    0 as final_credit
+                FROM
+                    account_move_line aml
+                    LEFT JOIN account_account aa ON aml.account_id = aa.id
+                    LEFT JOIN res_partner p ON aml.partner_id = p.id
+                    LEFT JOIN (
+                        SELECT
+                            partner_id,
+                            account_id,
+                            SUM(debit) AS total_debit_before,
+                            SUM(credit) AS total_credit_before
+                        FROM
+                            account_move_line
+                        WHERE
+                            date < '{start_date}' 
+                            AND (display_type NOT IN ('line_section', 'line_note') OR display_type IS NULL)
+                            AND parent_state = 'posted'
+                            AND (company_id IS NULL OR company_id ={company_id})
+                        GROUP BY
+                            partner_id, account_id
+                    ) pd ON aml.partner_id = pd.partner_id AND aml.account_id = pd.account_id
+                WHERE
+                    (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
+                    AND aml.parent_state = 'posted'
+                    AND (aml.company_id IS NULL OR aml.company_id ={company_id})
+                    AND aml.date BETWEEN '{start_date}' AND '{end_date}'
+                    and aml.account_id = {account_ids}
+                GROUP BY
+                    aml.account_id, aa.name, p.id, p.name, pd.total_debit_before, pd.total_credit_before
+                ORDER BY
+                    aa.name, p.name;
+            """
+
 
         # Construct the SQL to fetch all relevant account moves
-        account_move_sql = f"""
-            SELECT
-            aa.name,
-            aa.code,
-                min(aml.id) AS id,
-                count(aml.id) AS account_id_count,
-                min(aml.date) AS date,
-                sum(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.debit ELSE 0 END) AS period_debit,
-                sum(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.credit ELSE 0 END) AS period_credit,
-                sum(CASE WHEN aml.date <'{start_date}' THEN aml.debit ELSE 0 END) AS before_period_debit,
-                sum(CASE WHEN aml.date < '{start_date}' THEN aml.credit ELSE 0 END) AS before_period_credit,
-                 sum(aml.debit) AS final_debit,
-            sum(aml.credit) AS final_credit,
-                aml.account_id
-            FROM
-                account_move_line aml
-            LEFT JOIN
-                account_account aa ON aml.account_id = aa.id
-            LEFT JOIN
-                res_company rc ON aa.company_id = rc.id
-            WHERE
-                (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
-                AND aml.parent_state = 'posted'
-                AND (aml.company_id IS NULL OR aml.company_id IN (%s))
-                {account_ids_clause}
-            GROUP BY
-                aml.account_id,
+        else:
+            account_move_sql = f"""
+                SELECT
                 aa.name,
-                aa.code
-            ORDER BY
-                aa.code
-        """
+                aa.code,
+                    min(aml.id) AS id,
+                    count(aml.id) AS account_id_count,
+                    min(aml.date) AS date,
+                    sum(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.debit ELSE 0 END) AS period_debit,
+                    sum(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.credit ELSE 0 END) AS period_credit,
+                    sum(CASE WHEN aml.date <'{start_date}' THEN aml.debit ELSE 0 END) AS before_period_debit,
+                    sum(CASE WHEN aml.date < '{start_date}' THEN aml.credit ELSE 0 END) AS before_period_credit,
+                     sum(aml.debit) AS final_debit,
+                sum(aml.credit) AS final_credit,
+                    aml.account_id
+                FROM
+                    account_move_line aml
+                LEFT JOIN
+                    account_account aa ON aml.account_id = aa.id
+                LEFT JOIN
+                    res_company rc ON aa.company_id = rc.id
+                WHERE
+                    (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
+                    AND aml.parent_state = 'posted'
+                    AND (aml.company_id IS NULL OR aml.company_id IN (%s))
+                    {account_ids_clause}
+                GROUP BY
+                    aml.account_id,
+                    aa.name,
+                    aa.code
+                ORDER BY
+                    aa.code
+            """
         # Execute the SQL query
         self.env.cr.execute(account_move_sql, tuple(params))
         account_moves = self.env.cr.dictfetchall()
