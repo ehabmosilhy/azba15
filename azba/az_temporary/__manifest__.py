d8e4f6c5a555067858cd2c5dedd03e6300aedd39
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-01-27 12:09:30 +0200

    invoices_without_po

--- azba/az_temporary/__manifest__.py
+++ azba/az_temporary/__manifest__.py
@@ -1,9 +1,10 @@
 {
     "name": "Azbah - Temp",
-    "version": "15.0",
+    "version": "15.0.1",
     "license": "LGPL-3",
-    "depends": ["base","account"],
+    "depends": ["base","account", "stock","az_account_batch_purchase"],
     "data": [
         "views/bank_statement.xml",
+        "views/invoice_without_po.xml",
     ],
 }
