6dc068d9ff1833652ba5f2b494e77dd8698f7fa9
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-03-09 13:47:10 +0200

    added check credit balance to invoices

--- azba/az_sales_credit_limit/models/sale_order.py
+++ azba/az_sales_credit_limit/models/sale_order.py
@@ -1,22 +1,48 @@
 from odoo import models, api, _
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import ValidationError
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    def _check_credit_limit(self, partner, amount_total):
+        if partner.credit_limit_category_id:
+            if (amount_total + partner.total_due) > partner.credit_limit_category_id.credit_limit:
+                raise ValidationError(_("The total amount exceeds the customer's credit limit."))
+
     @api.model
     def create(self, vals):
-        result = super(SaleOrder, self).create(vals)
-        for order in result:
-            partner = order.partner_id
-            if partner.credit_limit_category_id and order.amount_total > partner.credit_limit_category_id.credit_limit:
-                raise ValidationError(_("The sales order amount exceeds the customer's credit limit."))
-        return result
+        order = super(SaleOrder, self).create(vals)
+        partner = order.partner_id
+        self._check_credit_limit(partner, order.amount_total)
+        return order
 
     def write(self, vals):
         result = super(SaleOrder, self).write(vals)
-        for order in self:
-            partner = order.partner_id
-            if partner.credit_limit_category_id and order.amount_total > partner.credit_limit_category_id.credit_limit:
-                raise ValidationError(_("The sales order amount exceeds the customer's credit limit."))
+        partner = self.partner_id
+        self._check_credit_limit(partner, self.amount_total)
+        return result
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def _check_credit_limit(self, partner, amount_total):
+        if partner.credit_limit_category_id:
+            if (amount_total + partner.total_due) > partner.credit_limit_category_id.credit_limit:
+                raise ValidationError(_("The total amount exceeds the customer's credit limit."))
+
+    @api.model
+    def create(self, vals):
+        invoice = super(AccountMove, self).create(vals)
+        if vals.get('move_type', '') in ['out_invoice', 'out_refund']:
+            partner = invoice.partner_id
+            self._check_credit_limit(partner, invoice.amount_total)
+        return invoice
+
+    def write(self, vals):
+        result = super(AccountMove, self).write(vals)
+        if self.filtered(lambda m: m.move_type in ['out_invoice', 'out_refund']):
+            partner = self.partner_id
+            self._check_credit_limit(partner, self.amount_total)
         return result
