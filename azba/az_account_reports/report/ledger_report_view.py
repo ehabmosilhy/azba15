d6607d957c61f54eed9f3dfe455f18deac68c53d
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-05-30 17:41:29 +0400

    fixed date range

--- azba/az_account_reports/report/ledger_report_view.py
+++ azba/az_account_reports/report/ledger_report_view.py
@@ -8,44 +8,39 @@ class LedgerReportTemplate(models.AbstractModel):
     _description = 'Ledger Report Template'
 
     def get_ledger_detail(self, data):
-        """
-        Retrieve detailed ledger information based on the provided data.
-
-        :param data: A dictionary containing filter criteria such as company_id, date range, and account_ids.
-        :return: A list of account moves with their aggregated debit and credit amounts.
-        """
-        # Extract filter criteria from the input data 📅
+        # Retrieve date range for filtering account moves 📅
         company_id = data.get('company_id')
         start_date = data.get('date_from')
         end_date = data.get('date_to')
         account_ids = data.get('account_ids')[0] if data.get('account_ids') else None
 
-        # Initialize SQL query parameters and clause for account IDs
-        account_ids_clause = ""
         params = [company_id]
 
-        # If specific account IDs are provided, construct a detailed SQL query 🧾
         if account_ids:
             account_move_sql = f"""
                 SELECT
                     aa.name AS account_name,
                     p.code,
                     p.name AS name,
-                    SUM(aml.debit) AS period_debit,
-                    SUM(aml.credit) AS period_credit,
+                    SUM(COALESCE(aml.debit, 0)) AS period_debit,
+                    SUM(COALESCE(aml.credit, 0)) AS period_credit,
                     COALESCE(pd.total_debit_before, 0) AS before_period_debit,
                     COALESCE(pd.total_credit_before, 0) AS before_period_credit,
-                    aml.account_id,
-                    0 as final_debit,
-                    0 as final_credit
+                    aa.id AS account_id,
+                    SUM(COALESCE(aml.debit, 0)) + COALESCE(pd.total_debit_before, 0) AS final_debit,
+                    SUM(COALESCE(aml.credit, 0)) + COALESCE(pd.total_credit_before, 0) AS final_credit
                 FROM
-                    account_move_line aml
-                    LEFT JOIN account_account aa ON aml.account_id = aa.id
-                    LEFT JOIN res_partner p ON aml.partner_id = p.id
+                    account_account aa
+                    LEFT JOIN res_partner p ON aa.id = p.id
+                    LEFT JOIN account_move_line aml ON aa.id = aml.account_id
+                    AND aml.date BETWEEN '{start_date}' AND '{end_date}'
+                    AND (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
+                    AND aml.parent_state = 'posted'
+                    AND (aml.company_id IS NULL OR aml.company_id = {company_id})
                     LEFT JOIN (
                         SELECT
-                            partner_id,
                             account_id,
+                            partner_id,
                             SUM(debit) AS total_debit_before,
                             SUM(credit) AS total_credit_before
                         FROM
@@ -56,25 +51,21 @@ class LedgerReportTemplate(models.AbstractModel):
                             AND parent_state = 'posted'
                             AND (company_id IS NULL OR company_id = {company_id})
                         GROUP BY
-                            partner_id, account_id
-                    ) pd ON aml.partner_id = pd.partner_id AND aml.account_id = pd.account_id
+                            account_id, partner_id
+                    ) pd ON aa.id = pd.account_id AND p.id = pd.partner_id
                 WHERE
-                    (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
-                    AND aml.parent_state = 'posted'
-                    AND (aml.company_id IS NULL OR aml.company_id = {company_id})
-                    AND aml.date BETWEEN '{start_date}' AND '{end_date}'
-                    AND aml.account_id = {account_ids}
+                    aa.id = {account_ids}
                 GROUP BY
-                    aml.account_id, aa.name, p.id, p.name, pd.total_debit_before, pd.total_credit_before
+                    aa.name, aa.id, p.code, p.name, pd.total_debit_before, pd.total_credit_before
                 ORDER BY
                     p.code;
             """
         else:
-            # If no specific account IDs are provided, construct a general SQL query 🧾
             account_move_sql = f"""
                 SELECT
                     aa.name,
                     aa.code,
+                    aa.id AS account_id,
                     MIN(aml.id) AS id,
                     COUNT(aml.id) AS account_id_count,
                     MIN(aml.date) AS date,
@@ -83,26 +74,39 @@ class LedgerReportTemplate(models.AbstractModel):
                     SUM(CASE WHEN aml.date < '{start_date}' THEN aml.debit ELSE 0 END) AS before_period_debit,
                     SUM(CASE WHEN aml.date < '{start_date}' THEN aml.credit ELSE 0 END) AS before_period_credit,
                     SUM(aml.debit) AS final_debit,
-                    SUM(aml.credit) AS final_credit,
-                    aml.account_id
+                    SUM(aml.credit) AS final_credit
                 FROM
-                    account_move_line aml
-                LEFT JOIN
-                    account_account aa ON aml.account_id = aa.id
-                LEFT JOIN
-                    res_company rc ON aa.company_id = rc.id
-                WHERE
-                    (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
+                    account_account aa
+                LEFT JOIN account_move_line aml ON aa.id = aml.account_id
+                    AND (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
                     AND aml.parent_state = 'posted'
                     AND (aml.company_id IS NULL OR aml.company_id IN (%s))
+                LEFT JOIN (
+                    SELECT
+                        account_id,
+                        SUM(debit) AS total_debit_before,
+                        SUM(credit) AS total_credit_before
+                    FROM
+                        account_move_line
+                    WHERE
+                        date < '{start_date}'
+                        AND (display_type NOT IN ('line_section', 'line_note') OR display_type IS NULL)
+                        AND parent_state = 'posted'
+                        AND (company_id IS NULL OR company_id IN (%s))
+                    GROUP BY
+                        account_id
+                ) pd ON aa.id = pd.account_id
+                WHERE
+                    aa.company_id IN (%s)
                 GROUP BY
-                    aml.account_id, aa.name, aa.code
+                    aa.name, aa.code, aa.id, pd.total_debit_before, pd.total_credit_before
                 ORDER BY
                     aa.code
             """
 
-        # Execute the SQL query to retrieve account moves 🚀
+        # Execute the SQL query 🚀
         self.env.cr.execute(account_move_sql, tuple(params))
+
         account_moves = self.env.cr.dictfetchall()
 
         # Initialize sums for the different periods 💰
