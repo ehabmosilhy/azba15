0e199fd14ddd659203ccc7eb4f5161cea7c1d847
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-06-01 09:32:21 +0400

    fixed excel report

--- azba/az_account_reports/wizard/ledger_report.py
+++ azba/az_account_reports/wizard/ledger_report.py
@@ -47,84 +47,84 @@ class LedgerReport(models.TransientModel):
 
         if account_ids:
             account_move_sql = f"""
-                  SELECT
-                      aa.name AS account_name,
-                      p.code,
-                      p.name AS name,
-                      SUM(aml.debit) AS period_debit,
-                      SUM(aml.credit) AS period_credit,
-                      COALESCE(pd.total_debit_before, 0) AS before_period_debit,
-                      COALESCE(pd.total_credit_before, 0) AS before_period_credit,
-                      aml.account_id,
-                      0 as final_debit,
-                      0 as final_credit
-                  FROM
-                      account_move_line aml
-                      LEFT JOIN account_account aa ON aml.account_id = aa.id
-                      LEFT JOIN res_partner p ON aml.partner_id = p.id
-                      LEFT JOIN (
-                          SELECT
-                              partner_id,
-                              account_id,
-                              SUM(debit) AS total_debit_before,
-                              SUM(credit) AS total_credit_before
-                          FROM
-                              account_move_line
-                          WHERE
-                              date < '{start_date}' 
-                              AND (display_type NOT IN ('line_section', 'line_note') OR display_type IS NULL)
-                              AND parent_state = 'posted'
-                              AND (company_id IS NULL OR company_id ={company_id})
-                          GROUP BY
-                              partner_id, account_id
-                      ) pd ON aml.partner_id = pd.partner_id AND aml.account_id = pd.account_id
-                  WHERE
-                      (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
-                      AND aml.parent_state = 'posted'
-                      AND (aml.company_id IS NULL OR aml.company_id ={company_id})
-                      AND aml.date BETWEEN '{start_date}' AND '{end_date}'
-                      and aml.account_id = {account_ids}
-                  GROUP BY
-                      aml.account_id, aa.name, p.id, p.name, pd.total_debit_before, pd.total_credit_before
-                  ORDER BY
-                      p.code;
-              """
+                      SELECT
+                          account.name AS account_name,
+                          partner.code,
+                          partner.name AS name,
+                          COALESCE(SUM(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.debit ELSE 0 END), 0) AS period_debit,
+                          COALESCE(SUM(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.credit ELSE 0 END), 0) AS period_credit,
+                          COALESCE(pd.total_debit_before, 0) AS before_period_debit,
+                          COALESCE(pd.total_credit_before, 0) AS before_period_credit,
+                          aml.account_id,
+                          0 as final_debit,
+                          0 as final_credit
+                      FROM
+                          account_move_line aml
+                          LEFT JOIN account_account account ON aml.account_id = account.id
+                          LEFT JOIN res_partner partner ON aml.partner_id = partner.id
+                          LEFT JOIN (
+                              SELECT
+                                  partner_id,
+                                  account_id,
+                                  SUM(debit) AS total_debit_before,
+                                  SUM(credit) AS total_credit_before
+                              FROM
+                                  account_move_line
+                              WHERE
+                                  date < '{start_date}' 
+                                  AND (display_type NOT IN ('line_section', 'line_note') OR display_type IS NULL)
+                                  AND parent_state = 'posted'
+                                  AND (company_id IS NULL OR company_id = {company_id})
+                              GROUP BY
+                                  partner_id, account_id
+                          ) pd ON aml.partner_id = pd.partner_id AND aml.account_id = pd.account_id
+                      WHERE
+                          (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
+                          AND aml.parent_state = 'posted'
+                          AND (aml.company_id IS NULL OR aml.company_id = {company_id})
+                          AND (aml.date BETWEEN '{start_date}' AND '{end_date}' OR aml.date < '{start_date}')
+                          AND aml.account_id = {account_ids}
+                      GROUP BY
+                          aml.account_id, account.name, partner.id, partner.name, pd.total_debit_before, pd.total_credit_before
+                      ORDER BY
+                          partner.code;
+                  """
 
-
-        # Construct the SQL to fetch all relevant account moves
+            # Construct the SQL to fetch all relevant account moves
         else:
             account_move_sql = f"""
-                  SELECT
-                  aa.name,
-                  aa.code,
-                      min(aml.id) AS id,
-                      count(aml.id) AS account_id_count,
-                      min(aml.date) AS date,
-                      sum(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.debit ELSE 0 END) AS period_debit,
-                      sum(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.credit ELSE 0 END) AS period_credit,
-                      sum(CASE WHEN aml.date <'{start_date}' THEN aml.debit ELSE 0 END) AS before_period_debit,
-                      sum(CASE WHEN aml.date < '{start_date}' THEN aml.credit ELSE 0 END) AS before_period_credit,
-                       sum(aml.debit) AS final_debit,
-                  sum(aml.credit) AS final_credit,
-                      aml.account_id
-                  FROM
-                      account_move_line aml
-                  LEFT JOIN
-                      account_account aa ON aml.account_id = aa.id
-                  LEFT JOIN
-                      res_company rc ON aa.company_id = rc.id
-                  WHERE
-                      (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
-                      AND aml.parent_state = 'posted'
-                      AND (aml.company_id IS NULL OR aml.company_id IN (%s))
-                      {account_ids_clause}
-                  GROUP BY
-                      aml.account_id,
-                      aa.name,
-                      aa.code
-                  ORDER BY
-                      aa.code
-              """
+                      SELECT
+                          aa.name,
+                          aa.code,
+                          MIN(aml.id) AS id,
+                          COUNT(aml.id) AS account_id_count,
+                          MIN(aml.date) AS date,
+                          SUM(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.debit ELSE 0 END) AS period_debit,
+                          SUM(CASE WHEN aml.date BETWEEN '{start_date}' AND '{end_date}' THEN aml.credit ELSE 0 END) AS period_credit,
+                          SUM(CASE WHEN aml.date < '{start_date}' THEN aml.debit ELSE 0 END) AS before_period_debit,
+                          SUM(CASE WHEN aml.date < '{start_date}' THEN aml.credit ELSE 0 END) AS before_period_credit,
+                          SUM(aml.debit) AS final_debit,
+                          SUM(aml.credit) AS final_credit,
+                          aml.account_id
+                      FROM
+                          account_move_line aml
+                      LEFT JOIN
+                          account_account aa ON aml.account_id = aa.id
+                      LEFT JOIN
+                          res_company rc ON aa.company_id = rc.id
+                      WHERE
+                          (aml.display_type NOT IN ('line_section', 'line_note') OR aml.display_type IS NULL)
+                          AND aml.parent_state = 'posted'
+                          AND (aml.company_id IS NULL OR aml.company_id IN (%s))
+                          {account_ids_clause}
+                          AND (aml.date BETWEEN '{start_date}' AND '{end_date}' OR aml.date < '{start_date}')
+                      GROUP BY
+                          aml.account_id,
+                          aa.name,
+                          aa.code
+                      ORDER BY
+                          aa.code
+                  """
         # Execute the SQL query
         self.env.cr.execute(account_move_sql, tuple(params))
         account_moves = self.env.cr.dictfetchall()
