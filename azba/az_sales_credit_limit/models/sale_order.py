7f50bc3ff7b30abeac61281cbcbd060871cd32ff
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-06-09 13:58:32 +0400

    Bypass credit limit check if the customer is paying in cash

--- azba/az_sales_credit_limit/models/sale_order.py
+++ azba/az_sales_credit_limit/models/sale_order.py
@@ -6,7 +6,10 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _check_credit_limit(self, partner, amount_total):
-        if partner.credit_limit_category_id:
+        # Check if the payment term is credit
+        is_credit_payment = self.payment_term_id and self.payment_term_id.name == 'Credit'  # Update this condition based on your actual credit payment term
+
+        if is_credit_payment and partner.credit_limit_category_id:
             if (amount_total + partner.total_due) > partner.credit_limit_category_id.credit_limit:
                 raise ValidationError(_("The total amount exceeds the customer's credit limit."))
 
@@ -28,7 +31,10 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def _check_credit_limit(self, partner, amount_total):
-        if partner.credit_limit_category_id:
+        # Check if the payment term is credit
+        is_credit_payment = self.invoice_payment_term_id and self.invoice_payment_term_id.name == 'Credit'  # Update this condition based on your actual credit payment term
+
+        if is_credit_payment and partner.credit_limit_category_id:
             if (amount_total + partner.total_due) > partner.credit_limit_category_id.credit_limit:
                 raise ValidationError(_("The total amount exceeds the customer's credit limit."))
 
@@ -37,12 +43,15 @@ class AccountMove(models.Model):
         invoice = super(AccountMove, self).create(vals)
         if vals.get('move_type', '') in ['out_invoice', 'out_refund']:
             partner = invoice.partner_id
-            self._check_credit_limit(partner, invoice.amount_total)
+            amount_total = invoice.amount_total
+            invoice._check_credit_limit(partner, amount_total)
         return invoice
 
     def write(self, vals):
         result = super(AccountMove, self).write(vals)
         if self.filtered(lambda m: m.move_type in ['out_invoice', 'out_refund']):
-            partner = self.partner_id
-            self._check_credit_limit(partner, self.amount_total)
+            for move in self:
+                partner = move.partner_id
+                amount_total = move.amount_total
+                move._check_credit_limit(partner, amount_total)
         return result
