9edefa22467123bc983902f06ded1fbbb77e0e28
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-08-08 12:35:18 +0400

    res_partner

--- azba/az_coupons/__manifest__.py
+++ azba/az_coupons/__manifest__.py
@@ -3,7 +3,11 @@
     "version": "15.0.1.0",
     "license": "LGPL-3",
     "category": "Sales",
-    "depends": ["base", "point_of_sale", "pos_coupon"],
+    "depends": ["base",
+                'mail',
+                'base_setup',
+                'contacts',
+                "point_of_sale", "pos_coupon"],
     "data": [
         'views/coupon_coupon.xml',
         'views/res_config_settings.xml',
