5234e474826b84071d4665068a2e8c6dcbe73b52
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-08-08 12:37:29 +0400

    res_partner

--- azba/az_coupons/models/__init__.py
+++ azba/az_coupons/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
+from . import res_partner
 from . import coupon_coupon
 from . import coupon_coupon_page
 from . import pos_order
 from . import settings
-from . import res_partner
