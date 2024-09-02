ec9afa169f071a71b51b7a36ef0b479f440a63d1
Ehab Mosilhy <ehabmosilhy@gmail.com>
2024-09-02 15:42:34 +0400

    pos reprint receipt

--- azba/azbah_pos/models/pos_order.py
+++ azba/azbah_pos/models/pos_order.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from datetime import datetime
-from uuid import uuid4
 import pytz
 
 from odoo import api, fields, models, tools, _
-from odoo.exceptions import ValidationError, UserError
 
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
     invoice_name = fields.Char(related='account_move.name')
 
+    def _export_for_ui(self, order):
+        timezone = pytz.timezone(self._context.get('tz') or self.env.user.tz or 'UTC')
+        res = super()._export_for_ui(order=order)
+        if res:
+            res.update({
+                'order_date': order.date_order.astimezone(timezone),
+                'invoice_number': order.account_move and order.account_move.name or "",
+            })
+        return res
